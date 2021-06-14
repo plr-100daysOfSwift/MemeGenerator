0fc2c5646b17203fd0425e99e5391ef19e2c4869
Paul Richardson <developer@photographerafoot.com>
2021-06-15 09:01:11 +1000

    CHG: Custom cell for meme
    add outlet for UIImageView
    disable estimated size for cell

---
+++ MemeGenerator/MemeCell.swift
@@ -0,0 +1,14 @@
+//
+//  MemeCell.swift
+//  MemeGenerator
+//
+//  Created by Paul Richardson on 15/06/2021.
+//
+
+import UIKit
+
+class MemeCell: UICollectionViewCell {
+    
+	@IBOutlet var imageView: UIImageView!
+	
+}
