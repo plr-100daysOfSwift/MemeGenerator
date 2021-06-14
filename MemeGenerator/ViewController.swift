bee10111588a38bb1b19286106758bbe1b5a77bd
Paul Richardson <developer@photographerafoot.com>
2021-06-15 08:28:34 +1000

    CHG: addMeme bar button and stub

--- MemeGenerator/ViewController.swift
+++ MemeGenerator/ViewController.swift
@@ -9,9 +9,29 @@ import UIKit
 
 class ViewController: UICollectionViewController {
 
+	var memes: [UIImage]!
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+
+		title = "Memes"
+		navigationController?.navigationBar.prefersLargeTitles = true
+		navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addMeme))
+
+		memes = [UIImage]()
+
+	}
+
+	@objc func addMeme() {
+
+		// pick an image
+
+		// prompt for header text
+
+		// prompt for footer text
+
+		// create image
+
 	}
 
 
